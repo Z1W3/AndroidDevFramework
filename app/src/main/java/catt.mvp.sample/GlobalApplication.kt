25ecef82f4d5309898c2131b71cbf87094d995ef
Zhi.Wei <249414003@qq.com>
2018-11-27 17:51:26 +0800

    1.添加签名配置demo 2.修复初始化顺序问题

--- app/src/main/java/catt.mvp.sample/GlobalApplication.kt
+++ app/src/main/java/catt.mvp.sample/GlobalApplication.kt
@@ -2,29 +2,31 @@ package catt.mvp.sample
 
 import android.app.Application
 import android.graphics.Color
+import android.util.Log.e
 import catt.compat.layout.internal.TargetScreenMetrics
 import com.umeng.analytics.MobclickAgent
 import com.umeng.commonsdk.UMConfigure
 import es.dmoral.toasty.Toasty
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
 class GlobalApplication : Application() {
-
+    private val _TAG by lazy { GlobalApplication::class.java.simpleName }
     override fun onCreate() {
         super.onCreate()
         initializeFrameworks()
     }
 
-    private fun initializeFrameworks(){
-        GlobalScope.launch(Dispatchers.IO) {
+    private fun initializeFrameworks() {
+        /*必须在主线程且必须第一个初始化*/TargetScreenMetrics.get().initContent(applicationContext, "2046x1536")
+        GlobalScope.launch(Dispatchers.IO, CoroutineStart.ATOMIC) {
             UMConfigure.init(applicationContext, BuildConfig.UMENG_APP_IDENTITY, BuildConfig.UMENG_CHANNEL, UMConfigure.DEVICE_TYPE_PHONE, BuildConfig.UMENG_SECRET_KEY)
             MobclickAgent.setScenarioType(applicationContext, MobclickAgent.EScenarioType.E_UM_NORMAL)
             MobclickAgent.setCatchUncaughtExceptions(false)
             CrashHandler.get().initContext(applicationContext)
-            TargetScreenMetrics.get().initContent(applicationContext, "2046x1536")
             Toasty.Config.getInstance().generatedConfig().apply()
         }
     }
